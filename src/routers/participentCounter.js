3a21045bf7b89b8330ba7d0b80c708655842453b
benmoyal1 <benmoyal2013@gmail.com>
2024-10-27 15:27:14 +0200

    updated

--- src/routers/participentCounter.js
+++ src/routers/participentCounter.js
@@ -2,19 +2,19 @@ import express from "express";
 import { kv } from "@vercel/kv";
 
 const router = express.Router();
-
-router.get("/participantCounter", async (req, res) => {
-  const { gender, exp } = req.query; // Extract gender and exp from query parameters
-  const participantsDict = await kv.get(exp);
+router.post("/participantCounter", async (req, res) => {
+  const { exp } = req.body; // Extract exp from request body
   try {
+    const participantsDict = await kv.get(exp); // Fetch participants data
     res.json({
       maleCounter: participantsDict.maleCounter,
-      femaleCounter : participantsDict.femaleCounter,
+      femaleCounter: participantsDict.femaleCounter,
     });
   } catch (error) {
     console.error("Error processing request:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
 
 export default router;
